0c023e5a6b3e55960f2c71ade07637a14a34cdab
alexfelipe <alexfelipevieira@gmail.com>
2017-09-21 15:50:19 -0300

    video 1.4

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,13 +2,23 @@ package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import br.com.alura.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 20,50",
+                "Economia - R$ 100,00")
+
+        val arrayAdapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(arrayAdapter)
     }
 
 }
