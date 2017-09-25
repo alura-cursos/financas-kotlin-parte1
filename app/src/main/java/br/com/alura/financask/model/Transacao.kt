fb30882c9aeec13e12a3bad2b2c9481781db7b63
alexfelipe <alexfelipevieira@gmail.com>
2017-09-25 10:56:48 -0300

    video 2.2 e 2.3

--- app/src/main/java/br/com/alura/financask/model/Transacao.kt
+++ app/src/main/java/br/com/alura/financask/model/Transacao.kt
@@ -6,15 +6,6 @@ import java.util.Calendar
 /**
  * Created by Alura on 22/09/2017.
  */
-class Transacao (valor: BigDecimal,
-                 categoria: String,
-                 data: Calendar){
-
-    private val valor: BigDecimal = valor
-    private val categoria: String = categoria
-    private val data: Calendar = data
-
-    fun getValor() : BigDecimal {
-        return valor
-    }
-}
+class Transacao (val valor: BigDecimal,
+                 val categoria: String,
+                 val data: Calendar)
