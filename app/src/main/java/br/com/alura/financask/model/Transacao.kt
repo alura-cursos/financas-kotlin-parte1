9fcd87ba7728445c79ebe99981c4c784c41e9e96
alexfelipe <alexfelipevieira@gmail.com>
2017-09-26 10:25:19 -0300

    video 3.2

--- app/src/main/java/br/com/alura/financask/model/Transacao.kt
+++ app/src/main/java/br/com/alura/financask/model/Transacao.kt
@@ -7,5 +7,6 @@ import java.util.Calendar
  * Created by Alura on 22/09/2017.
  */
 class Transacao (val valor: BigDecimal,
-                 val categoria: String,
-                 val data: Calendar)
+                 val categoria: String = "Indefinida",
+                 val tipo: Tipo,
+                 val data: Calendar = Calendar.getInstance())
